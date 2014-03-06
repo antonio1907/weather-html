3c34f8015c2af4cd4080a17ee942c2e546098355
Antonio José Ruiz Velázquez <taco.com@hotmail.com>
2014-03-06 18:12:42 +0100

    Update weather-html.py

--- weather-html.py
+++ weather-html.py
@@ -12,7 +12,7 @@ print """
 7. Malaga
 8. Sevilla
 
-'''
+"""
 ciudades={1:"Almeria", 2:"Cadiz", 3:"Cordoba", 4:"Granada", 5:"Huelva", 6:"Jaen", 7:"Malaga", 8:"Sevilla"}
 ciudad=int(raw_input("De que ciudad quieres saber la temperatura actual?:"))
 
